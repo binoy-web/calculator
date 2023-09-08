10ed82e526dc539d847813d43f12ef32f6591916
binoy-web <66207807+binoy-web@users.noreply.github.com>
2023-09-08 18:29:08 +0530

    three files are uploaded
    This respository is consisting of three files
    index.html 
    s1.js
    style.css

---
+++ s1.js
@@ -0,0 +1,28 @@
+const display=document.getElementById("display");
+document.getElementById("b1").addEventListener("click",function(){
+    display.innerText="1";
+});
+document.getElementById("b2").addEventListener("click",function(){
+    display.innerText="2";
+});
+document.getElementById("b3").addEventListener("click",function(){
+    display.innerText="3";
+});
+document.getElementById("b4").addEventListener("click",function(){
+    display.innerText="4";
+});
+document.getElementById("b5").addEventListener("click",function(){
+    display.innerText="5";
+});
+document.getElementById("b6").addEventListener("click",function(){
+    display.innerText="6";
+});
+document.getElementById("b7").addEventListener("click",function(){
+    display.innerText="7";
+});
+document.getElementById("b8").addEventListener("click",function(){
+    display.innerText="8";
+});
+document.getElementById("b9").addEventListener("click",function(){
+    display.innerText="9";
+});
